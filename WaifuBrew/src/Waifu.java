1215b2bc9001a38311a0b1c061fd02eb96a8ddbb
Nam Kim <abe8132@live.ca>
2017-12-02 11:43:02 -0800

    Haha

--- WaifuBrew/src/Waifu.java
+++ WaifuBrew/src/Waifu.java
@@ -1,15 +1,31 @@
 public class Waifu {
-     int weight; //it's kg
-     int height; //it's cm
+    int weight; //it's kg
+    int height; //it's cm
+    int[] threeSizes = new int[3];
+    String name;
+    String mood;
 
     public Waifu(){
         weight = 0;
         height = 0;
+        threeSizes[0] = 0;
+        threeSizes[1] = 0;
+        threeSizes[2] = 0;
+        name = "";
+        mood = "normal";
     }
 
-    public Waifu(int weight, int height) {
+    public Waifu(String name, int weight, int height) {
+        this.name = name;
         this.weight = weight;
         this.height = height;
+    }
+
+    public Waifu(String name, int weight, int height, int[] threeSizes) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.threeSizes = threeSizes;
     }
 
     public int getWeight() {
@@ -26,5 +42,17 @@ public class Waifu {
 
     public void setHeight(int Height) {
         this.height = Height;
+    }
+
+    public int[] getThreeSizes() {
+        return threeSizes;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getMood() {
+        return this.mood;
     }
 }
