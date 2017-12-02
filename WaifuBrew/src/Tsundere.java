e428420b89186a1988d74f6d4d0a1538e9bdaf78
Nam Kim <abe8132@live.ca>
2017-12-02 11:45:19 -0800

    Gaia

---
+++ WaifuBrew/src/Tsundere.java
@@ -0,0 +1,12 @@
+public class Tsundere extends Waifu{
+
+    public Tsundere () {
+    }
+
+    public Tsundere(String name, int weight, int height) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+
+}
