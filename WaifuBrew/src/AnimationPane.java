d0bcd8150d6a6205c98be779f4447bf393209115
Nam Wook Kim <abe8132@live.ca>
2018-04-09 01:41:48 -0700

    Playing around with AffineTransform(Op)
    Apparently Java has a problem where it cuts off top and left of the
    image when rotating, so I need huge calculation inorder to fix it.
    (Quadrant calc)

--- WaifuBrew/src/AnimationPane.java
+++ WaifuBrew/src/AnimationPane.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,10 +14,12 @@ public class AnimationPane extends JPanel {
     private BufferedImage scrollingImage;
     private int xPos = 0;
     private int direction = 1;
+    private double rotationDeg = 0;
 
     public AnimationPane() {
         try {
             scrollingImage = ImageIO.read(new File("src/resources/icon.jpg"));
+            /*
             Timer timer = new Timer(1, new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -31,6 +35,24 @@ public class AnimationPane extends JPanel {
                 }
 
             });
+            */
+
+            Timer timer = new Timer(1, e -> {
+                // xPos += direction;
+                rotationDeg++;
+                System.out.println("Rotation: " + rotationDeg);
+                if (xPos + scrollingImage.getWidth() > getWidth()) {
+                    xPos = getWidth() - scrollingImage.getWidth();
+                    direction *= -1;
+                    System.out.println("REEEEEEEEEEEEEEEEEE");
+                } else if (xPos < 0) {
+                    xPos = 0;
+                    direction *= -1;
+                    System.out.println("AHHHHHHHHHHHHHHHHHH");
+                }
+                repaint();
+            });
+
             timer.setRepeats(true);
             timer.setCoalesce(true);
             timer.start();
@@ -41,15 +63,21 @@ public class AnimationPane extends JPanel {
 
     @Override
     public Dimension getPreferredSize() {
+        // System.out.println("scrollingImageWidth: "+scrollingImage.getWidth() + " scrollingImageHeight: " + scrollingImage.getHeight());
         return scrollingImage == null ? super.getPreferredSize() : new Dimension(scrollingImage.getWidth() * 4, scrollingImage.getHeight());
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         int y = getHeight() - scrollingImage.getHeight();
-        g.drawImage(scrollingImage, xPos, y, this);
+        // g.drawImage(scrollingImage, xPos, y, this);
+        AffineTransform rotation = AffineTransform.getRotateInstance(Math.toRadians(rotationDeg), scrollingImage.getWidth() / 2, scrollingImage.getHeight() / 2);
+        AffineTransformOp op = new AffineTransformOp(rotation, AffineTransformOp.TYPE_BICUBIC);
+        // Apparently I can save the image through dst.
+        g.drawImage(op.filter(scrollingImage, null), 600, 300, this);
+
+
 
     }
 
--- WaifuBrew/src/ImagePanel.java
+++ WaifuBrew/src/ImagePanel.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+// Copos De Nieve - Christopher Slaski
+
 @SuppressWarnings("serial")
 public class ImagePanel extends JPanel implements ActionListener {
     private BufferedImage backgroundPicture;
