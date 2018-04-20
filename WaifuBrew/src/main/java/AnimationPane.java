2ee9e55864efde90b4934173bdf582a9e6fc7523
Nam Wook Kim <abe8132@live.ca>
2018-04-20 01:33:36 -0700

    Project converted to Maven
    Added JavaXT as dependency for images.

---
+++ WaifuBrew/src/main/java/AnimationPane.java
@@ -0,0 +1,81 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class AnimationPane extends JPanel {
+
+    private BufferedImage scrollingImage;
+    private int xPos = 0;
+    private int direction = 1;
+    private double rotationDeg = 0;
+
+    public AnimationPane() {
+        try {
+            scrollingImage = ImageIO.read(new File("src/main/java/resources/icon.jpg"));
+            javaxt.io.Image image = new javaxt.io.Image("src/main/java/resources/icon.jpg");
+            System.out.println(image.getWidth() + "x" + image.getHeight());
+
+
+            Timer timer = new Timer(1, new ActionListener() {
+                // @Override
+                public void actionPerformed(ActionEvent e) {
+                    xPos += direction;
+                    rotationDeg += direction;
+                    if (xPos + scrollingImage.getWidth() > getWidth()) {
+                        xPos = getWidth() - scrollingImage.getWidth();
+                        direction *= -1;
+                        rotationDeg *= -1;
+                        System.out.println("REEEEEEEEEEEEEEEEEE");
+                    } else if (xPos < 0) {
+                        xPos = 0;
+                        rotationDeg = 0;
+                        direction *= -1;
+                        rotationDeg *= -1;
+                        System.out.println("AHHHHHHHHHHHHHHHHHH");
+                    }
+                    repaint();
+                }
+            });
+
+
+            timer.setRepeats(true);
+            timer.setCoalesce(true);
+            timer.start();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        // System.out.println("scrollingImageWidth: "+scrollingImage.getWidth() + " scrollingImageHeight: " + scrollingImage.getHeight());
+        return scrollingImage == null ? super.getPreferredSize() : new Dimension(scrollingImage.getWidth() * 4, scrollingImage.getHeight());
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int y = getHeight() - scrollingImage.getHeight();
+
+
+        // g.drawImage(scrollingImage, xPos, y, this);
+        AffineTransform rotation = AffineTransform.getRotateInstance(Math.toRadians(rotationDeg), scrollingImage.getWidth() / 2, scrollingImage.getHeight() / 2);
+        AffineTransformOp op = new AffineTransformOp(rotation, AffineTransformOp.TYPE_BICUBIC);
+
+
+
+        // Apparently I can save the image through dst.
+        g.drawImage(op.filter(scrollingImage, null), xPos, getSize().height / 2 - (scrollingImage.getHeight() / 2 ), this);
+
+
+
+    }
+
+}
---
+++ WaifuBrew/src/main/java/StartScreen.java
@@ -0,0 +1,87 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class StartScreen extends JPanel implements ActionListener {
+
+    private BufferedImage backgroundPicture;
+    private BufferedImage start_button;
+    private BufferedImage config_button;
+    private BufferedImage exit_button;
+
+    private int buttonY = 600;
+
+    private String resourcePath = "src/main/java/resources/";
+
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+        System.out.println("Hi");
+    }
+
+    public StartScreen() {
+        try {
+            backgroundPicture = ImageIO.read(new File(resourcePath + "start.png"));
+            start_button = ImageIO.read(new File(resourcePath + "startscreen_start_button.png"));
+            config_button = ImageIO.read(new File(resourcePath + "startscreen_config_button.png"));
+            exit_button = ImageIO.read(new File(resourcePath + "startscreen_exit_button.png"));
+        } catch (IOException e) {
+            System.out.println("File failure in StartScreen class");
+            e.printStackTrace(); // Wall of error
+            System.exit(-1);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundPicture != null) {
+            // I want to centre the image that is 960:640 to widescreen format, but do not want to stretch. I will zoom in!
+
+            // Not centered tho... lo
+
+            // int b =(int)Math.round(a);
+            if(getPreferredSize(backgroundPicture).width < getSize().width) {
+                /*
+                System.out.println("BG width: "+getPreferredSize(backgroundPicture).width);
+                System.out.println("Window width: "+getSize().width);
+                System.out.println("BG height: "+getPreferredSize(backgroundPicture).height);
+                System.out.println("Window height: "+getSize().height);
+                System.out.println("Calculated height: "+(getSize().width/getPreferredSize(backgroundPicture).width) * getPreferredSize(backgroundPicture).height);
+                System.out.println("Ratio: "+(getSize().width/(double)getPreferredSize(backgroundPicture).width));
+                */
+                if((getSize().width/(double)getPreferredSize(backgroundPicture).width) * getPreferredSize(backgroundPicture).height < getSize().height) {
+                    // TODO: Change x0, y0 if picture is changed.
+                    g.drawImage(backgroundPicture,0,0,(int)Math.round(getSize().height / (double)getPreferredSize(backgroundPicture).height * getPreferredSize(backgroundPicture).width), getSize().height, this);
+                    // System.out.println("Using height priority");
+                }
+                else {
+                    g.drawImage(backgroundPicture, 0, 0, getSize().width,(int)Math.round(getSize().width / (double)getPreferredSize(backgroundPicture).width * getPreferredSize(backgroundPicture).height), this);
+                    // System.out.println("Using width priority");
+                }
+            }
+
+            // Add rest of the menu elements
+
+            g.drawImage(start_button, (getSize().width / 4) - (getPreferredSize(start_button).width / 2), buttonY - (getPreferredSize(start_button).height / 2), getPreferredSize(start_button).width, getPreferredSize(start_button).height,this);
+            g.drawImage(config_button,(getSize().width / 4) * 2 - (getPreferredSize(config_button).width / 2), buttonY - (getPreferredSize(config_button).height / 2) , getPreferredSize(config_button).width, getPreferredSize(config_button).height,  this);
+            g.drawImage(exit_button,(getSize().width / 4) * 3 - (getPreferredSize(exit_button).width / 2), buttonY - (getPreferredSize(exit_button).height / 2), getPreferredSize(exit_button).width, getPreferredSize(exit_button).height,  this);
+
+        }
+    }
+
+    public Dimension getPreferredSize(BufferedImage a) {
+        if (a != null) {
+            int width = a.getWidth();
+            int height = a.getHeight();
+            return new Dimension(width, height);
+        }
+        return super.getPreferredSize();
+    }
+}
