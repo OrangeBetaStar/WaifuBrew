838f304f49b0055dc57084ec56968444693ce8a9
Nam Wook Kim <abe8132@live.ca>
2018-06-29 15:36:16 -0700

    Creating custom sliders
    Midway of implementation

--- WaifuBrew/src/main/java/start/ConfigPane.java
+++ WaifuBrew/src/main/java/start/ConfigPane.java
@@ -1,5 +1,7 @@
 package start;
 
+import com.sun.java.swing.Painter;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -7,6 +9,7 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 @SuppressWarnings("serial")
 public class ConfigPane extends JPanel implements ActionListener {
@@ -14,10 +17,8 @@ public class ConfigPane extends JPanel implements ActionListener {
     private BufferedImage backgroundPicture;
     private javaxt.io.Image back_button;
 
-    // TODO:
-    private javaxt.io.Image dialogueTransBack;
-    private javaxt.io.Image dialogueTransKnob;
     private JSlider dialogueTransSlider;
+    private UIDefaults sliderDefaults;
 
     private int backButtonX = 1100;
     private int backButtonY = 600;
@@ -39,20 +40,94 @@ public class ConfigPane extends JPanel implements ActionListener {
             // TODO: Add buttons (back, toggles, sound system?... etc)
 
             back_button = new javaxt.io.Image(RESOURCE_PATH + "config_back_button.png");
-
-            // TODO:
-            dialogueTransBack = new javaxt.io.Image(RESOURCE_PATH + "black.png");
-            dialogueTransKnob = new javaxt.io.Image(RESOURCE_PATH + "black.png");
             dialogueTransSlider = new JSlider(0, 100, dialogueTransparency);
 
             addMouseListener(handler);
             addMouseMotionListener(handler);
+
+            sliderDefaults = new UIDefaults();
+            sliderDefaults.put("Slider.thumbWidth", 20);
+            sliderDefaults.put("Slider.thumbHeight", 20);
+
+            System.out.println("Outside of the things");
+
+            // sliderDefaults.put(“Slider:SliderThumb[Pressed].backgroundPainter”ONLY WORKS WHEN PRESSED
+
+            sliderDefaults.put("Slider:SliderTrack[Enabled].backgroundPainter", new Painter<JSlider>() {
+
+                public void paint(Graphics2D g, JSlider c, int w, int h) {
+                    System.out.println("Middle");
+                    int arc         = 10;
+                    int trackHeight = 8;
+                    int trackWidth  = w - 2;
+                    int fillTop     = 4;
+                    int fillLeft    = 1;
+
+                    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                    g.setStroke(new BasicStroke(1.5f));
+                    g.setColor(Color.GRAY);
+                    g.fillRoundRect(fillLeft, fillTop, trackWidth, trackHeight, arc, arc);
+
+                    int fillBottom = fillTop + trackHeight;
+                    int fillRight  = xPositionForValue(
+                            c.getValue(), c,
+                            new Rectangle(fillLeft, fillTop, trackWidth, fillBottom - fillTop));
+
+                    g.setColor(Color.ORANGE);
+                    g.fillRect(fillLeft + 1, fillTop + 1, fillRight - fillLeft, fillBottom - fillTop);
+
+                    g.setColor(Color.WHITE);
+                    g.drawRoundRect(fillLeft, fillTop, trackWidth, trackHeight, arc, arc);
+                }
+            });
+
+            for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(laf.getName())){
+                    try {
+                        UIManager.setLookAndFeel(laf.getClassName());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            for (Map.Entry<Object, Object> entry : UIManager.getLookAndFeelDefaults().entrySet()) {
+                if ((entry.getKey()).toString().startsWith("Slider")){
+                    System.out.println(entry.getKey() +" = "+ entry.getValue());
+                }
+            }
+
+            dialogueTransSlider.putClientProperty("Nimbus.Overrides.InheritDefaults",false);
+            dialogueTransSlider.putClientProperty("Nimbus.Overrides",sliderDefaults);
+
+
+            add(dialogueTransSlider);
+
 
         } catch (IOException e) {
             System.out.println("File failure in Config class");
             e.printStackTrace(); // Wall of error
             System.exit(-1);
         }
+    }
+
+    protected int xPositionForValue(int value, JSlider slider, Rectangle trackRect) {
+        int min = slider.getMinimum();
+        int max = slider.getMaximum();
+        int trackLength = trackRect.width;
+        double valueRange = (double) max - (double) min;
+        double pixelsPerValue = (double) trackLength / valueRange;
+        int trackLeft = trackRect.x;
+        int trackRight = trackRect.x + (trackRect.width - 1);
+        int xPosition;
+
+        xPosition = trackLeft;
+        xPosition += Math.round(pixelsPerValue * ((double) value - min));
+
+        xPosition = Math.max(trackLeft, xPosition);
+        xPosition = Math.min(trackRight, xPosition);
+
+        return xPosition;
     }
 
     @Override
@@ -91,9 +166,9 @@ public class ConfigPane extends JPanel implements ActionListener {
             tempBackButton.setOpacity(20);
         }
         g.drawImage(tempBackButton.getBufferedImage(), (backButtonX - (getPreferredSize(tempBackButton.getBufferedImage()).width / 2)), (backButtonY - (getPreferredSize(tempBackButton.getBufferedImage()).height / 2)), getPreferredSize(tempBackButton.getBufferedImage()).width, getPreferredSize(tempBackButton.getBufferedImage()).height, this);
-        dialogueTransBack.resize(200,10);
+
         g.drawString("Diologue Bar Transparency", 100, 180);
-        g.drawImage(dialogueTransBack.getBufferedImage(), 150, 200, this);
+
         // TODO: https://jasperpotts.com/blog/2008/08/skinning-a-slider-with-nimbus/
 
         // Save resource (in mouseEntered) - Didn't work
