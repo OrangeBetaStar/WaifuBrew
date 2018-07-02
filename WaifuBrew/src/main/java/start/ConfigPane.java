368bca8985f8805652474ab1b9209a8a83a19b28
BetaStar <abe8132@live.ca>
2018-07-02 14:35:45 -0700

    Just going to make custom one
    I give up on java's laf since slider still doesn't look good and too limiting.

--- WaifuBrew/src/main/java/start/ConfigPane.java
+++ WaifuBrew/src/main/java/start/ConfigPane.java
@@ -1,24 +1,19 @@
 package start;
 
-import com.sun.java.swing.Painter;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.plaf.synth.SynthLookAndFeel;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
 @SuppressWarnings("serial")
 public class ConfigPane extends JPanel implements ActionListener {
 
     private BufferedImage backgroundPicture;
     private javaxt.io.Image back_button;
-
-    private JSlider dialogueTransSlider;
-    private UIDefaults sliderDefaults;
 
     private int backButtonX = 1100;
     private int backButtonY = 600;
@@ -40,68 +35,28 @@ public class ConfigPane extends JPanel implements ActionListener {
             // TODO: Add buttons (back, toggles, sound system?... etc)
 
             back_button = new javaxt.io.Image(RESOURCE_PATH + "config_back_button.png");
-            dialogueTransSlider = new JSlider(0, 100, dialogueTransparency);
+
+            try {
+                SynthLookAndFeel laf = new SynthLookAndFeel();
+                System.out.println(RESOURCE_PATH + "lof.xml");
+
+                laf.load(this.getClass().getResourceAsStream(RESOURCE_PATH + "lof.xml"), this.getClass());
+                System.out.println("test");
+                UIManager.setLookAndFeel(laf);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            add(new JSlider() {{
+                setOrientation(JSlider.HORIZONTAL);
+                setMinimum(0);
+                setMaximum(100);
+                setVisible(true);
+            }});
 
             addMouseListener(handler);
             addMouseMotionListener(handler);
-
-            sliderDefaults = new UIDefaults();
-            sliderDefaults.put("Slider.thumbWidth", 20);
-            sliderDefaults.put("Slider.thumbHeight", 20);
-
-            System.out.println("Outside of the things");
-
-            // sliderDefaults.put(“Slider:SliderThumb[Pressed].backgroundPainter”ONLY WORKS WHEN PRESSED
-
-            sliderDefaults.put("Slider:SliderTrack[Enabled].backgroundPainter", new Painter<JSlider>() {
-
-                public void paint(Graphics2D g, JSlider c, int w, int h) {
-                    System.out.println("Middle");
-                    int arc         = 10;
-                    int trackHeight = 8;
-                    int trackWidth  = w - 2;
-                    int fillTop     = 4;
-                    int fillLeft    = 1;
-
-                    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                    g.setStroke(new BasicStroke(1.5f));
-                    g.setColor(Color.GRAY);
-                    g.fillRoundRect(fillLeft, fillTop, trackWidth, trackHeight, arc, arc);
-
-                    int fillBottom = fillTop + trackHeight;
-                    int fillRight  = xPositionForValue(
-                            c.getValue(), c,
-                            new Rectangle(fillLeft, fillTop, trackWidth, fillBottom - fillTop));
-
-                    g.setColor(Color.ORANGE);
-                    g.fillRect(fillLeft + 1, fillTop + 1, fillRight - fillLeft, fillBottom - fillTop);
-
-                    g.setColor(Color.WHITE);
-                    g.drawRoundRect(fillLeft, fillTop, trackWidth, trackHeight, arc, arc);
-                }
-            });
-
-            for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(laf.getName())){
-                    try {
-                        UIManager.setLookAndFeel(laf.getClassName());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            for (Map.Entry<Object, Object> entry : UIManager.getLookAndFeelDefaults().entrySet()) {
-                if ((entry.getKey()).toString().startsWith("Slider")){
-                    System.out.println(entry.getKey() +" = "+ entry.getValue());
-                }
-            }
-
-            dialogueTransSlider.putClientProperty("Nimbus.Overrides.InheritDefaults",false);
-            dialogueTransSlider.putClientProperty("Nimbus.Overrides",sliderDefaults);
-
-
-            add(dialogueTransSlider);
 
 
         } catch (IOException e) {
@@ -111,24 +66,6 @@ public class ConfigPane extends JPanel implements ActionListener {
         }
     }
 
-    protected int xPositionForValue(int value, JSlider slider, Rectangle trackRect) {
-        int min = slider.getMinimum();
-        int max = slider.getMaximum();
-        int trackLength = trackRect.width;
-        double valueRange = (double) max - (double) min;
-        double pixelsPerValue = (double) trackLength / valueRange;
-        int trackLeft = trackRect.x;
-        int trackRight = trackRect.x + (trackRect.width - 1);
-        int xPosition;
-
-        xPosition = trackLeft;
-        xPosition += Math.round(pixelsPerValue * ((double) value - min));
-
-        xPosition = Math.max(trackLeft, xPosition);
-        xPosition = Math.min(trackRight, xPosition);
-
-        return xPosition;
-    }
 
     @Override
     protected void paintComponent(Graphics g) {
